4caed84e57352ae4bcf26b185b49c471375e27de
Diego Armando Simbaña <diegoarmandosim@outlook.com>
2024-02-15 22:38:52 +0100

    Se empieza los modulos del segundo año.

---
+++ DESARROLLO WEB CLIENTE/TEMA.01/EJERCICIOS/Video xampp - javascript inscrutado - javascript externo - noscript/Ejemplo/ejemplo.js
@@ -0,0 +1,7 @@
+   res.style.backgroundColor="blue";
+   function sumar(){
+      var sum1=parseInt(num1.value);
+      var sum2=parseInt(num2.value);
+      res.value=sum1+sum2;
+      res.style.backgroundColor="green";
+   }
