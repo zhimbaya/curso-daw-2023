c3659a2ef896733d8b0dcbb0ac0a25b7b7d8dcb6
Diego Armando Simbaña <diegoarmandosim@outlook.com>
2024-03-22 19:47:59 +0100

    Se esta con el curso de javaScript.

---
+++ DESARROLLO WEB CLIENTE/EJERCICIOS/01.Introduccion/variables.js
@@ -0,0 +1,31 @@
+/*
+Tipos de datos que podemos guardar en las varialbes:
+- string
+- number
+- boolean
+- object
+- function
+//
+- null
+- indefined
+*/
+//variables
+
+var edad = 27;
+console.log(edad);
+
+let nombre = 'Carlos';
+const correo = 'correo@correo.com';
+
+let telefono, pais, id;
+
+const resultado = 4 + 4;
+console.log(resultado);
+
+const nombre1 = 'Diego ';
+const nombre2 = 'Armando';
+const nombreCompleto = nombre1 + nombre2;
+console.log(nombreCompleto);
+
+let miVariable = 'texto';
+miVariable = 7;
