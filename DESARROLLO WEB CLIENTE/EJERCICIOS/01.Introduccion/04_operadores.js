05b52281d3f5b8810dad9c5e98d049c75390db57
Diego Armando Simbaña <diegoarmandosim@outlook.com>
2024-03-24 17:01:44 +0100

    Se actualiza los cursos.

---
+++ DESARROLLO WEB CLIENTE/EJERCICIOS/01.Introduccion/04_operadores.js
@@ -0,0 +1,35 @@
+// operadores aritmeticos
+/*
+= asignación
++ - * / % ++ --
+*/
+
+const result = 10 + 10;
+const resul1 = 10 % 3;
+
+let numero = 1;
+numero++;
+
+console.log(numero);
+
+//OPERADORE DE ASIGNACIÓN
+// += -= *= /= %=
+
+/* Operadores de comparación
+==
+===
+!=
+!==
+>
+<
+>=
+<=
+? operador ternario
+
+*/
+
+const resultado = 20 >= 20;
+console.log(resultado);
+
+const result2 = 7 > 1 ? 'El primer valor es mayor que el segundo' : 'El sgundo valor es mayor que el';
+console.log(result2);
