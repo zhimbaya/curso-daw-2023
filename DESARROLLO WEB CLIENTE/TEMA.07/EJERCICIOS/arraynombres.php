0c077c41f42fdf89daa90759a33ff44cb63d565c
Diego Armando Simbaña <diegoarmandosim@outlook.com>
2024-06-18 06:56:52 +0200

    Se esta con ajax.

---
+++ DESARROLLO WEB CLIENTE/TEMA.07/EJERCICIOS/arraynombres.php
@@ -0,0 +1,12 @@
+<?php
+//Plugins
+//PHP Extension Pack, PHP Server.
+//Configuración > php > settings.json > pegar ruta de php de xampp
+//
+/*
+"phpserver.phpPath": "/Applications/XAMPP/xamppfiles/bin/php-8.2.4",
+"phpserver.phpConfigPath": "/Applications/XAMPP/xamppfiles/etc/php.ini",
+"php.validate.executablePath": "/Applications/XAMPP/xamppfiles/bin/php-8.2.4",
+*/
+echo "qyual";
+?>
